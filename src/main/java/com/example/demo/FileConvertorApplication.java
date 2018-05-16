fe42f0f072923bbbff14b13de4fa7f6af84db830
Nan Tian <tiannan209@hotmail.com>
2018-05-16 10:19:39 +0800

    A demo for JSON reader and XML convertor

---
+++ src/main/java/com/example/demo/FileConvertorApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileConvertorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileConvertorApplication.class, args);
+    }
+}
