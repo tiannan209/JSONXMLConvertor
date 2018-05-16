6e21f9a4fcaf06edece1084f945f533ef66206b0
Nan Tian <tiannan209@hotmail.com>
2018-05-16 15:11:19 +0800

    A demo for JSON reader and XML convertor

--- src/main/java/com/example/demo/convertorControl.java
+++ src/main/java/com/example/demo/convertorControl.java
@@ -1,92 +1,83 @@
 package com.example.demo;
 
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
-import net.sf.json.xml.XMLSerializer;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.awt.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.StringReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Iterator;
-import java.util.Map;
 
 @RestController
 public class convertorControl {
 
     @RequestMapping(value = "/convertor", method = RequestMethod.GET)
     public String getXml(){
-        //JSONArray jsonArray = extractSourceText("jsonExample.txt", "properties");
-        //for(int i = 0; i < jsonArray.size(); i++){
-        //    System.out.println(jsonArray.get(i).toString());
-        //}
-        //extractSourceText("jsonExample.txt", "properties");
-        return getXmlSource(readTxt("jsonExample.txt"));
+        String jsonStr = readTxt("jsonExample.txt");
+        //saveXML(xmlSource);
+        return getXmlStr(jsonStr);
     }
 
-    private String getXmlSource(String jsonStr){
-        String xmlSource = null;
+    private String getXmlStr(String jsonStr){
+        String xmlStr = null;
 
         try{
-            //JSONArray jsonArray = JSONArray.fromObject(jsonStr);
-            //System.out.println(jsonArray.size());
             JSONObject jsonObject = JSONObject.fromObject(jsonStr);
             JSONArray jsonArray = jsonObject.getJSONArray("items");
             System.out.println(jsonArray.size());
 
-            Iterator<JSONArray> iterator = jsonArray.iterator();
+            Iterator iterator = jsonArray.iterator();
+            /* configuration object is used to store property list */
             JSONObject configuration = null;
-            StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.append("<configuration>\n");
+            //StringBuilder stringBuilder = new StringBuilder();
+            //stringBuilder.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+            //stringBuilder.append("<configuration>\n")
             while(iterator.hasNext()){
                 JSONObject temp = JSONObject.fromObject(iterator.next());
                 System.out.println(temp.get("href"));
                 System.out.println(temp.get("Config"));
                 System.out.println(temp.get("properties"));
                 configuration = JSONObject.fromObject(temp.get("properties"));
-                System.out.println(configuration.size());
-                Iterator<JSONObject.Entry<String, String>> configItr = configuration.entrySet().iterator();
-                while(configItr.hasNext()){
-                    JSONObject.Entry<String, String> property = configItr.next();
-                    System.out.println("<name>" + property.getKey().trim() + "</name>");
-                    System.out.println("<value>" + property.getValue().trim() + "</value>");
-                    stringBuilder.append("   <property>\n");
-                    stringBuilder.append("      <name>" + property.getKey().trim() + "</name>\n");
-                    stringBuilder.append("      <value>" + property.getValue().trim() + "</value>\n");
-                    stringBuilder.append("   </property>\n");
-                }
             }
-            stringBuilder.append("</configuration>");
-            xmlSource = stringBuilder.toString();
-
-            //jsonObject.getJSONArray("properties");
-//            JsonFactory factory =  new JsonFactory();
-//            ObjectMapper mapper = new ObjectMapper(factory);
-//            JsonNode rootNode = mapper.readTree(jsonStr);
-//
-//            Iterator<Map.Entry<String,JsonNode>> fieldsIterator = rootNode.fields();
-//            while (fieldsIterator.hasNext()) {
-//
-//                Map.Entry<String,JsonNode> field = fieldsIterator.next();
-//                System.out.println("Key: " + field.getKey() + "\tValue:" + field.getValue());
-//            }
-            //XMLSerializer xmlSerializer = new XMLSerializer();
-            //xmlSource = xmlSerializer.write(configuration);
-
-
-            /* print Json array test*/
+            xmlStr = getXmlStr(configuration);
+            saveXML(xmlStr);
         }catch(Exception ex){
             System.err.println("Error to generate xml format string: " + ex.getMessage());
         }
 
-        return xmlSource;
+        return xmlStr;
+    }
+
+    private String getXmlStr(JSONObject propertiesJson){
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+        stringBuilder.append("<configuration>\n");
+        Iterator<JSONObject.Entry<String, String>> configItr = propertiesJson.entrySet().iterator();
+        while(configItr.hasNext()){
+            JSONObject.Entry<String, String> property = configItr.next();
+            System.out.println("<name>" + property.getKey().trim() + "</name>");
+            System.out.println("<value>" + property.getValue().trim() + "</value>");
+            stringBuilder.append("   <property>\n");
+            stringBuilder.append("      <name>").append(property.getKey().trim()).append("</name>\n");
+            stringBuilder.append("      <value>").append(property.getValue().trim()).append("</value>\n");
+            stringBuilder.append("   </property>\n");
+        }
+        stringBuilder.append("</configuration>");
+        return stringBuilder.toString();
+    }
+
+    private void saveXML(String xmlSource) {
+        try{
+            FileWriter fw = new FileWriter("F:/configurations.xml");
+            fw.write(xmlSource);
+            fw.close();
+        }catch(IOException ex){
+            System.err.println("Fail to create a XML file: " + ex.getMessage());
+        }
     }
 
     private String readTxt(String doc){
@@ -112,24 +103,5 @@ public class convertorControl {
         //System.out.println(content);
 
         return content;
-    }
-
-    private void extractSourceText(String doc, String targetStr){
-        BufferedReader bufferedReader;
-
-        try{
-            bufferedReader = new BufferedReader(new FileReader(doc));
-
-            String strLine;
-            while((strLine = bufferedReader.readLine()) != null){
-                JSONObject jsonObject = JSONObject.fromObject(strLine);
-                System.out.println(jsonObject.toString());
-            }
-
-        }catch(Exception ex){
-            System.err.println("Fail to extract JSON array: " + ex.getMessage());
-        }
-
-        return;
     }
 }
